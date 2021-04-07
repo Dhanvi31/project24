542712355662a456e7fc77e852638230ee37467b
Dhanvi31 <79754778+Dhanvi31@users.noreply.github.com>
2021-04-07 18:35:53 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -1,23 +1,25 @@
 class DustBin {  
-    constructor( x, y, width, height){
-        var options = {
-            'isStatic' : true
-        }
-
-        this.bodyb = Bodies.rectangle(x,y, width, height,options );
-	    World.add(world, this.bodyb);
-	    this.bodyl= Bodies.rectangle((x-(width/2)), (y-(height*2)), height, width/2 ,options);
-	    World.add(world, this.bodyl);
-	    this.bodyr = Bodies.rectangle((x+(width/2)), (y-(height*2)), height, width/2 ,options);
-        World.add(world, this.bodyr);
-        this.bodyb.width = width
-        this.bodyb.height = height 
-        
-   }
-   display(){
-       fill("white");
-       rect(this.bodyb.position.x, this.bodyb.position.y,this.bodyb.width, this.bodyb.height)
-       rect((this.bodyb.position.x-(this.bodyb.width/2)), (this.bodyb.position.y-(this.bodyb.height*(2*(this.bodyb.width/100)))), this.bodyb.height, this.bodyb.width/2)
-       rect((this.bodyb.position.x+(this.bodyb.width/2)), (this.bodyb.position.y-(this.bodyb.height*(2*(this.bodyb.width/100)))), this.bodyb.height, this.bodyb.width/2)
-   }
-}
+    constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(255);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
