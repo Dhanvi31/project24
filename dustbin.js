72f3976061ceef3c83e98b47fea587f6d6d26b03
Dhanvi31 <79754778+Dhanvi31@users.noreply.github.com>
2021-04-08 15:43:31 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -1,25 +1,24 @@
-class DustBin {  
-    constructor(x, y, width, height) {
-    var options = {
-        'restitution':0.8,
-        'friction':1.0,
-        'density':1.0
+class Dustbin {
+    constructor(x,y,width,height)
+    {
+
+
+        var dustbin_options={
+            isStatic: true
+        }
+        this.body= Bodies.rectangle(x,y,width,height,dustbin_options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+
     }
-    this.body = Bodies.rectangle(x, y, width, height, options);
-    this.width = width;
-    this.height = height;
-    
-    World.add(world, this.body);
-  }
-  display(){
-    var pos =this.body.position;
-    var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
-    rectMode(CENTER);
-    fill(255);
-    rect(0, 0, this.width, this.height);
-    pop();
-  }
-};
+
+    display()
+    {
+        var pos= this.body.position;
+        rectMode(CENTER)
+        
+        fill("blue")
+        rect(pos.x,pos.y, this.width,this.height)
+    }
+}
